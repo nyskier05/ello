9e8eb10e04e2fdf92191f01e8d54d04b5c84d8d8
NY Skier <nyskier05@gmail.com>
2016-08-04 00:55:50 -0400

    Add cellphone format validation error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   	:recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, :last_name, :date_of_birth, :height, :church, :church_status, :interests, :non_negotiables, :cellphone, :presence => true
-	validates :cellphone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }, :uniqueness => true
+	validates :cellphone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format, must be in ###-####-### format" }, :uniqueness => true
 	validates :is_female, inclusion: { in: [ true, false ] }
 	#validates_inclusion_of :is_female, in: [true, false]
 end
