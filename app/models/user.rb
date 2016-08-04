c847145a069ba66620e1dfe788e0d4abe7361db0
NY Skier <nyskier05@gmail.com>
2016-08-04 18:47:47 -0400

    Add avatar validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "250x250>", thumb: "100x100>", small: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  validates :first_name, :last_name, :date_of_birth, :height, :church, :church_status, :interests, :non_negotiables, :cellphone, :presence => true
+  validates :first_name, :last_name, :date_of_birth, :height, :church, :church_status, :interests, :non_negotiables, :cellphone, :avatar, :presence => true
 	validates :cellphone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format, must be in ###-####-### format" }, :uniqueness => true
 	validates :is_female, inclusion: { in: [ true, false ] }
 	#validates_inclusion_of :is_female, in: [true, false]
